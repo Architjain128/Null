162a7a6cbf06e62fff1e80bbca06c87182af8169
Pulkit Gupta <pulkit.g@students.iiit.ac.in>
2023-03-31 14:24:21 +0530

    added two nodes

--- frontend/src/components/Canvas/Canvas.jsx
+++ frontend/src/components/Canvas/Canvas.jsx
@@ -1,36 +1,60 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import ReactFlow, {
   MiniMap,
   Controls,
   Background,
   useNodesState,
   useEdgesState,
+  applyNodeChanges,
+  applyEdgeChanges,
   addEdge,
 } from 'reactflow';
 import ParentNode from './parentNodes.jsx';
 import CustomNodeEdit from './customNodesEdit.jsx';
 // import CustomNodeView from './customNodesView.js';
 
+const rfStyle = {
+  backgroundColor: '#B8CEFF',
+};
 import 'reactflow/dist/style.css';
-import { nodes as initialNodes, edges as initialEdges } from './data.jsx';
+import { nodes as initialNodes } from './data.jsx';
 
 const nodeTypes = {
   child: CustomNodeEdit,
   parent: ParentNode,
 //   childView: CustomNodeView,
 };
+const initialEdges = [
+  { id: 'edge-1', sourceHandle: '1_0_r', targetHandle: '2_0_l'},
+  { id: 'edge-2', sourceHandle: '1_1_r', targetHandle: '2_2_l'},
+];
 
 export default function Canvas() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
 
-  const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
     [setEdges]
   );
+  const onConnect = useCallback(
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
+  // const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  // const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+
+  // const onConnect = useCallback(
+  //   (params) => setEdges((eds) => addEdge(params, eds)),
+  //   [setEdges]
+  // );
 
   return (
-    <div style={{ width: '100%', height: '100%' }}>
+    // <div style={{ width: '100%', height: '100%' }}>
       <ReactFlow
         nodes={nodes}
         edges={edges}
@@ -38,11 +62,10 @@ export default function Canvas() {
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         nodeTypes={nodeTypes}
-      >
-        <Controls />
-        <MiniMap />
-        <Background variant="dots" gap={12} size={1} />
-      </ReactFlow>
-    </div>
+        fitView
+      // style={rfStyle}
+      />
+       
+    // </div>
   );
 }
--- frontend/src/components/Canvas/data.jsx
+++ frontend/src/components/Canvas/data.jsx
@@ -64,6 +64,68 @@ export const nodes = [
       width: 224,
     },
   },
+  {
+    id: '2',
+    type: 'parent',
+    position: { x: 100, y: 100 },
+    data: {
+      tableName: 'empId',
+      link_to_file: '/',
+      attribute_count : 3, 
+    },
+    style: {
+      width: 244,
+      height: 380,
+    },
+  },
+  {
+    id: '2_0',
+    type: 'child',
+    position: { x: 10, y: 50 },
+    data: {
+      name: '_id',
+      type: 'Obj',
+      key: true,
+    },
+    parentNode: '2',
+    extent: 'parent',
+    draggable: false,
+    style: {
+      width: 224,
+    },
+  },
+  {
+    id: '2_1',
+    type: 'child',
+    position: { x: 10, y: 150 },
+    data: {
+      name: 'Name',
+      type: 'string',
+      key: false,
+    },
+    draggable: false,
+    parentNode: '2',
+    extent: 'parent',
+    style: {
+      width: 224,
+    },
+  },
+  {
+    id: '2_2',
+    type: 'child',
+    position: { x: 10, y: 250 },
+    data: {
+      name: 'val',
+      type: 'num',
+      key: false,
+    },
+    parentNode: '2',
+    extent: 'parent',
+    draggable: false,
+    style: {
+      width: 224,
+    },
+  },
 ];
 
-export const edges = [];
+// export const edges = [];
